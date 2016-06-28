3512cd5efc01b91f028a55377778fd22468854ea
wangJunBo <wangJunBo@qq.com>
2016-06-29 01:13:45 +0800

    模拟异步加载数据

--- 私人通讯录/私人通讯录/ListViewController.swift
+++ 私人通讯录/私人通讯录/ListViewController.swift
@@ -10,20 +10,46 @@ import UIKit
 
 class ListViewController: UITableViewController {
 
+    var personList = [Person]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        loadData { (list) in
+            print(list)
+            
+            self.personList += list
+            
+            self.tableView.reloadData()
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // 模拟异步加载数据  使用闭包  要传一个数据数组回去 所以闭包参数为一个数组
+    private func loadData(completion:(list:[Person])->()) {
+        
+        // 异步
+        DispatchQueue.global().async { 
+            print("正在努力加载")
+            Thread.sleep(forTimeInterval: 1)
+            
+            var arrayM = [Person]()
+            for i in 0..<20 {
+                // 创建模型
+                let p = Person()
+                // 设置模型属性
+                p.name = "person - \(i)"
+                p.phone = "1836613" + String(format: "%04d", arc4random_uniform(10000))
+                p.title = "boss - \(i)"
+                
+                arrayM.append(p)
+            }
+            
+           // 主线程回调  执行闭包
+            DispatchQueue.main.async(execute: { 
+                completion(list: arrayM)
+            })
+        }
     }
 
-    
 }
