032f640aad4e062ee3a6fca75881867a2c2f92ae
wangJunBo <wangJunBo@qq.com>
2016-06-29 00:37:59 +0800

    定义模型

---
+++ 私人通讯录/私人通讯录/Person.swift
@@ -0,0 +1,17 @@
+//
+//  Person.swift
+//  私人通讯录
+//
+//  Created by 王军波 on 16/6/29.
+//  Copyright © 2016年 王军波. All rights reserved.
+//
+
+import UIKit
+
+class Person: NSObject {
+    
+    var name: String?
+    var phone: String?
+    var title: String?
+    
+}
