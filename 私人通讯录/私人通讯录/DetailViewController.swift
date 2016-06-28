1918bc8d80a1581f323766c5e006f3330b435c60
wangJunBo <wangJunBo@qq.com>
2016-06-29 00:35:13 +0800

    设置界面,准备目录

---
+++ 私人通讯录/私人通讯录/DetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailViewController.swift
+//  私人通讯录
+//
+//  Created by 王军波 on 16/6/29.
+//  Copyright © 2016年 王军波. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+
+}
